fd8b7fe0ba0171001046f711835f168ba7c2f6e2
lukyr <luca.rossif010@gmail.com>
2017-06-16 15:48:34 +0200

    White Icons

--- app/src/main/java/com/example/alessio/tesi/MainActivity.java
+++ app/src/main/java/com/example/alessio/tesi/MainActivity.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +42,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_settings) {
             return true;
         }
 
