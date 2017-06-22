78438c89254d430ca0c85436598be2824544799f
Fabio <Masso793@gmail.com>
2017-06-22 22:24:03 +0200

    menu 2nd activity + OK button working

--- app/src/main/java/com/example/alessio/tesi/SessionSettingsActivity.java
+++ app/src/main/java/com/example/alessio/tesi/SessionSettingsActivity.java
@@ -1,12 +1,57 @@
 package com.example.alessio.tesi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class SessionSettingsActivity extends Activity {
+public class SessionSettingsActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button endedConfig;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.session_settings_activity);
+
+        endedConfig = (Button) findViewById(R.id.endedConfig);
+
+        endedConfig.setOnClickListener(this);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.endedConfig:
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+        }
+    }
+
 }
